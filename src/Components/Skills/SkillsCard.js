8ee9e22a6eb3ead7c9b2b26848cf00653b8a46a4
jradziejewski <261309@student.pwr.edu.pl>
2022-03-10 22:35:40 +0100

    Added some styling to those cards, so they actually look like cards now

--- src/Components/Skills/SkillsCard.js
+++ src/Components/Skills/SkillsCard.js
@@ -1,22 +1,22 @@
 import React from "react"
+import "./Skills.css"
 import ReactCardFlip from "react-card-flip"
 
 export const Card = ({ skill }) => {
   const [isFlipped, setIsFlipped] = React.useState(false)
-
   return (
     <ReactCardFlip
       key={skill.title}
       isFlipped={isFlipped}
-      flipdirection="horizontal"
+      flipDirection="horizontal"
     >
-      <div className="front" onMouseEnter={() => setIsFlipped((prev) => !prev)}>
+      <div className="card-front" onClick={() => setIsFlipped((prev) => !prev)}>
         <section>
           <h3>{skill.title}</h3>
           <h3>{skill.subtitle}</h3>
         </section>
       </div>
-      <div onMouseLeave={() => setIsFlipped((prev) => !prev)}>
+      <div className="card-back" onClick={() => setIsFlipped((prev) => !prev)}>
         {skill.description}
       </div>
     </ReactCardFlip>
